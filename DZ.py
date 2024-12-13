31e8e814602c69c16553e5d7c6de49a98c17ceaf
22011990-AAP <antonanufriev@icloud.com>
2024-12-13 19:40:08 +0000

    Add files via upload

---
+++ DZ.py
@@ -0,0 +1,2 @@
+print("to be\n\n\n\t\t\t or not\n\n\n\t\t\t\t\t\t\t\t to be\n\n\n\n\t\t\t\t\t\t\t\t\t\t \ William Shekspeer \ ") 
+print("\t Грудень 2024\nПН   Вт   Ср   Чт   Пт   Сб   Вс\n                              1 \n2     3    4    5    6    7   8\n9    10    11    12    13    14\n15   16    17    18    19    20\n21   22    23    24    25    26\n27   28    29    30    31")
